667b9c4ac016bdb9e1bdf4579566d94d8b168509
Jonas Weber <jonas@jole.dev>
2023-11-10 18:22:37 +0100

    Nvim adjustments

--- .config/nvim/lua/jole/core/autocommands.lua
+++ .config/nvim/lua/jole/core/autocommands.lua
@@ -4,3 +4,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
         vim.highlight.on_yank()
     end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "tex" },
+    callback = function()
+        vim.cmd.setlocal({ "spell", "spelllang=en_us" })
+    end,
+})
