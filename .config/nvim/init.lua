182e1b674dcb031a82887a80ce8ffe017d3a44a8
Jonas Weber <jonas@jole.dev>
2023-11-11 14:50:52 +0100

    More new nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,15 +1,46 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+vim.opt.backup = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cursorline = true
 vim.opt.expandtab = true
 vim.opt.ignorecase = true
+vim.opt.mouse = "a"
 vim.opt.relativenumber = true
+vim.opt.scrolloff = 8
 vim.opt.shiftwidth = 4
+vim.opt.signcolumn = "yes"
 vim.opt.smartcase = true
 vim.opt.softtabstop = 4
-vim.opt.signcolumn = "yes"
+vim.opt.swapfile = false
 vim.opt.tabstop = 4
+vim.opt.undofile = true
+vim.opt.undolevels = 10000
+vim.opt.updatetime = 50
 vim.opt.wrap = false
+
+-- Center view when navigating
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Move lines
+vim.keymap.set("n", "<A-j>", "<cmd>m .+1<cr>==")
+vim.keymap.set("n", "<A-k>", "<cmd>m .-2<cr>==")
+vim.keymap.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi")
+vim.keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi")
+vim.keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv")
+vim.keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv")
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = vim.api.nvim_create_augroup("HighlightYank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -43,12 +74,49 @@ local plugins = {
             local lspconfig = require("lspconfig")
 
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+            local on_attach = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({
+                        group = augroup,
+                        buffer = bufnr
+                    })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({ bufnr = bufnr, async = true })
+                        end
+                    })
+                end
+            end
+
+            lspconfig.docker_compose_language_service.setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+            })
+
+            lspconfig.dockerls.setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+            })
 
             lspconfig.gopls.setup({
-                capabilities = capabilities
+                capabilities = capabilities,
+                on_attach = on_attach,
+                settings = {
+                    gopls = {
+                        analyses = {
+                            unusedparams = true,
+                            composites = false,
+                        },
+                    },
+                },
             })
+
             lspconfig.lua_ls.setup({
                 capabilities = capabilities,
+                on_attach = on_attach,
                 settings = {
                     Lua = {
                         workspace = {
@@ -60,14 +128,35 @@ local plugins = {
                     }
                 }
             })
-            lspconfig.pyright.setup({
-                capabilities = capabilities
-            })
-            lspconfig.rust_analyzer.setup({
-                capabilities = capabilities
+
+            lspconfig.pylsp.setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
             })
 
-            vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+            lspconfig.rust_analyzer.setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+                settings = {
+                    ["rust-analyzer"] = {
+                        cargo = {
+                            features = "all",
+                        },
+                    },
+                },
+            })
+
+            lspconfig.solargraph.setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+            })
+
+            lspconfig.standardrb.setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+            })
+
+            vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
             vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
             vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
             vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
@@ -79,19 +168,19 @@ local plugins = {
 
                     local opts = { buffer = ev.buf }
                     vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                    vim.keymap.set("n", "gd", require("telescope.builtin").lsp_definitions, opts)
                     vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-                    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                    vim.keymap.set("n", "gi", require("telescope.builtin").lsp_implementations, opts)
                     vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
                     vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
                     vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
                     vim.keymap.set("n", "<leader>wl", function()
                         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
                     end, opts)
-                    vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+                    vim.keymap.set("n", "<leader>D", require("telescope.builtin").lsp_type_definitions, opts)
                     vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
                     vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-                    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                    vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, opts)
                     vim.keymap.set("n", "<leader>f", function()
                         vim.lsp.buf.format({ async = true })
                     end, opts)
@@ -202,6 +291,7 @@ local plugins = {
             local builtin = require("telescope.builtin")
             telescope.load_extension("fzf")
             vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>fr", builtin.oldfiles, {})
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
             vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
             vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
@@ -213,28 +303,130 @@ local plugins = {
         config = function()
             ---@diagnostic disable-next-line: missing-fields
             require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    "c",
-                    "lua",
-                    "query",
-                    "vim",
-                    "vimdoc",
-                },
                 sync_install = false,
                 auto_install = false,
                 highlight = {
                     enable = true,
                     additional_vim_regex_highlighting = false,
-                }
+                },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<C-space>",
+                        node_incremental = "<C-space>",
+                        scope_incremental = false,
+                        node_decremental = "<A-space>",
+                    },
+                },
+                ensure_installed = {
+                    "bash",
+                    "c",
+                    "css",
+                    "dockerfile",
+                    "embedded_template",
+                    "gitignore",
+                    "go",
+                    "gomod",
+                    "gosum",
+                    "html",
+                    "latex",
+                    "javascript",
+                    "json",
+                    "lua",
+                    "markdown",
+                    "markdown_inline",
+                    "python",
+                    "query",
+                    "ruby",
+                    "rust",
+                    "scss",
+                    "sql",
+                    "toml",
+                    "typescript",
+                    "vim",
+                    "vimdoc",
+                    "yaml",
+                },
             })
         end
     },
     {
+        "nvim-treesitter/nvim-treesitter-context",
+    },
+    {
         'stevearc/dressing.nvim',
         opts = {},
+    },
+    {
+        'numToStr/Comment.nvim',
+        opts = {},
+        lazy = false,
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+            "3rd/image.nvim",
+        },
+        config = function()
+            local neotree = require("neo-tree")
+
+            neotree.setup({
+                close_if_last_window = true,
+                git_status_async = false,
+                filesystem = {
+                    follow_current_file = { enabled = true },
+                    use_libuv_file_watcher = true,
+                },
+                window = {
+                    mappings = {
+                        ["<space>"] = "none",
+                        ["h"] = function(state)
+                            local node = state.tree:get_node()
+                            if node:is_expanded() then
+                                node:collapse(node:get_id())
+                                require("neo-tree.ui.renderer").redraw(state)
+                            else
+                                require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+                            end
+                        end,
+                        ["l"] = "open",
+                    },
+                },
+            })
+
+            vim.keymap.set("n", "<leader>e", "<cmd>Neotree float reveal<CR>", { desc = "Toggle Neotree" })
+        end,
+    },
+    {
+        "j-hui/fidget.nvim",
+        opts = {},
+    },
+    {
+        "kdheepak/lazygit.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        config = function()
+            vim.keymap.set("n", "<leader>gg", vim.cmd.LazyGit, { desc = "Open LazyGit" })
+        end,
     }
 }
 
-local opts = {}
+local opts = {
+    install = {
+        colorscheme = { "catppuccin" },
+    },
+    checker = {
+        enabled = true,
+        notify = false,
+    },
+    change_detection = {
+        notify = false,
+    },
+}
 
 require("lazy").setup(plugins, opts)
