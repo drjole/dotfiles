515eebb3cde2cffc419083c272394be8a9d41699
Jonas Weber <jonas@jole.dev>
2023-11-16 20:22:44 +0100

    Neovim telescope stuff

--- .config/nvim/lua/jole/plugins/telescope.lua
+++ .config/nvim/lua/jole/plugins/telescope.lua
@@ -20,6 +20,8 @@ return {
         vim.keymap.set("n", "<leader>fr", builtin.oldfiles, {})
         vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
         vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-        vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+        vim.keymap.set("n", "<leader>fh", builtin.grep_string, {})
+        vim.keymap.set("n", "<leader>/", builtin.current_buffer_fuzzy_find, {})
+        vim.keymap.set("n", "z=", builtin.spell_suggest)
     end,
 }
