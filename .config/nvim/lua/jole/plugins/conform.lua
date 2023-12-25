6204ee1fe89ef23607bbe6dea9989866a83670e9
Jonas Weber <jonas@jole.dev>
2023-12-25 13:51:52 +0100

    Conditional async formatting in NeoVim

--- .config/nvim/lua/jole/plugins/conform.lua
+++ .config/nvim/lua/jole/plugins/conform.lua
@@ -1,33 +1,50 @@
 return {
     "stevearc/conform.nvim",
-    opts = {
-        formatters_by_ft = {
-            bash = { "shfmt" },
-            css = { "prettier" },
-            eruby = { "erb_format" },
-            gohtmltmpl = { "prettier" },
-            gotexttmpl = { "prettier" },
-            html = { "prettier" },
-            javascript = { "prettier" },
-            json = { "prettier" },
-            markdown = { "prettier" },
-            ruby = { "standardrb" },
-            scss = { "prettier" },
-            sh = { "shfmt" },
-            template = { "prettier" },
-            typescript = { "prettier" },
-            yaml = { "prettier" },
-            ["_"] = { "trim_whitespace" },
-        },
-        formatters = {
-            erb_format = {
-                prepend_args = { "--print-width", "120" },
+    config = function()
+        local slow_format_filetypes = {}
+        require("conform").setup({
+            formatters_by_ft = {
+                bash = { "shfmt" },
+                css = { "prettier" },
+                eruby = { "erb_format" },
+                gohtmltmpl = { "prettier" },
+                gotexttmpl = { "prettier" },
+                html = { "prettier" },
+                javascript = { "prettier" },
+                json = { "prettier" },
+                markdown = { "prettier" },
+                ruby = { "standardrb" },
+                scss = { "prettier" },
+                sh = { "shfmt" },
+                template = { "prettier" },
+                typescript = { "prettier" },
+                yaml = { "prettier" },
+                ["_"] = { "trim_whitespace" },
             },
-        },
-        format_on_save = {
-            lsp_fallback = true,
-            async = false,
-            timeout_ms = 5000,
-        },
-    },
+            formatters = {
+                erb_format = {
+                    prepend_args = { "--print-width", "120" },
+                },
+            },
+            format_on_save = function(bufnr)
+                if slow_format_filetypes[vim.bo[bufnr].filetype] then
+                    return
+                end
+                local function on_format(err)
+                    if err and err:match("timeout$") then
+                        slow_format_filetypes[vim.bo[bufnr].filetype] = true
+                    end
+                end
+
+                return { timeout_ms = 200, lsp_fallback = true }, on_format
+            end,
+
+            format_after_save = function(bufnr)
+                if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+                    return
+                end
+                return { lsp_fallback = true }
+            end,
+        })
+    end,
 }
