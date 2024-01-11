b8e91b5a02c62838ebf01409e21bd83c35ba84b0
Jonas Weber <jonas@jole.dev>
2024-01-12 00:28:24 +0100

    Dotfiles and stow stuff

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ STOW_FOLDERS=$(find . -maxdepth 1 -type d -not -path . -not -path ./.git -printf
 
 echo "$STOW_FOLDERS" | while read stow_folder; do
   echo "Stowing $stow_folder"
-  stow -R $stow_folder
+  stow -D $stow_folder
+  stow $stow_folder
 done
