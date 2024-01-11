c2b342c92d1e87da1f5686c6dd921fdb2469f601
Jonas Weber <jonas@jole.dev>
2024-01-12 00:23:37 +0100

    Move to using stow for dotfiles

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+STOW_FOLDERS=$(find . -maxdepth 1 -type d -not -path . -not -path ./.git -printf '%P\n' | sort)
+
+echo "$STOW_FOLDERS" | while read stow_folder; do
+  echo "Stowing $stow_folder"
+  stow -R $stow_folder
+done
