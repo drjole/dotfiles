7cb5acc03085c605767354661b590cff7962bb9e
Jonas Weber <jonas@jole.dev>
2024-01-12 00:31:46 +0100

    Use stow --adopt

--- setup.sh
+++ setup.sh
@@ -4,6 +4,5 @@ STOW_FOLDERS=$(find . -maxdepth 1 -type d -not -path . -not -path ./.git -printf
 
 echo "$STOW_FOLDERS" | while read stow_folder; do
   echo "Stowing $stow_folder"
-  stow -D $stow_folder
-  stow $stow_folder
+  stow --adopt $stow_folder
 done
