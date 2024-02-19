bfbde59013643933341ab66b0a56250a0495f2b2
Jonas Weber <jonas@jole.dev>
2024-02-19 12:48:12 +0100

    Improve telescope find_files picker to also find hidden files except for the files inside the .git directories

--- .config/nvim/lua/jole/plugins/telescope.lua
+++ .config/nvim/lua/jole/plugins/telescope.lua
@@ -20,7 +20,7 @@ return {
             },
             pickers = {
                 find_files = {
-                    hidden = true,
+                    find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
                 },
             },
         }
