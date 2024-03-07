a846f0ad1ef3bf86b73d3e59f0492eb6de708959
Jonas Weber <jonas@jole.dev>
2024-03-07 12:13:05 +0100

    Use keys feature of lazy for telescope.nvim

--- .config/nvim/lua/jole/plugins/telescope.lua
+++ .config/nvim/lua/jole/plugins/telescope.lua
@@ -3,19 +3,28 @@ return {
     branch = "0.1.x",
     dependencies = {
         "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope-fzf-native.nvim",
+        {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "make",
+        },
         "folke/trouble.nvim",
+    },
+    keys = {
+        { "<leader>ff", function() require("telescope.builtin").find_files() end,                desc = "Find files" },
+        { "<leader>fr", function() require("telescope.builtin").oldfiles() end,                  desc = "Find recent files" },
+        { "<leader>fg", function() require("telescope.builtin").live_grep() end,                 desc = "Find in files" },
+        { "<leader>fb", function() require("telescope.builtin").buffers() end,                   desc = "Find buffers" },
+        { "<leader>fh", function() require("telescope.builtin").help_tags() end,                 desc = "Find help tags" },
+        { "<leader>/",  function() require("telescope.builtin").current_buffer_fuzzy_find() end, desc = "Find in buffer" },
+        { "z=",         function() require("telescope.builtin").spell_suggest() end,             desc = "Spell suggest" },
     },
     config = function()
         local telescope = require("telescope")
-        local builtin = require("telescope.builtin")
-        local trouble = require("trouble.providers.telescope")
-
         telescope.setup {
             defaults = {
                 mappings = {
-                    i = { ["<C-q>"] = trouble.open_with_trouble },
-                    n = { ["<C-q>"] = trouble.open_with_trouble },
+                    i = { ["<C-q>"] = require("trouble.providers.telescope").open_with_trouble },
+                    n = { ["<C-q>"] = require("trouble.providers.telescope").open_with_trouble },
                 },
             },
             pickers = {
@@ -27,15 +36,6 @@ return {
                 },
             },
         }
-
         telescope.load_extension("fzf")
-
-        vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-        vim.keymap.set("n", "<leader>fr", builtin.oldfiles, {})
-        vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-        vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-        vim.keymap.set("n", "<leader>fh", builtin.grep_string, {})
-        vim.keymap.set("n", "<leader>/", builtin.current_buffer_fuzzy_find, {})
-        vim.keymap.set("n", "z=", builtin.spell_suggest)
     end,
 }
