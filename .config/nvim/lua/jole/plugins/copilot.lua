d8a0d3b6acae7f0448eccf9596da92770f3d677b
Jonas Weber <jonas@jole.dev>
2024-03-21 16:40:20 +0100

    Don't lazy load Copilot in Nvim and remove luasnip plugin file

--- .config/nvim/lua/jole/plugins/copilot.lua
+++ .config/nvim/lua/jole/plugins/copilot.lua
@@ -1,10 +1,8 @@
 return {
     "github/copilot.vim",
-    event = "InsertEnter",
-    keys = {
-        { "<C-j>", 'copilot#Accept("<CR>")', mode = "i", desc = "Accept Copilot suggestion", expr = true, replace_keycodes = false },
-    },
     init = function()
         vim.g.copilot_no_tab_map = true
+        vim.keymap.set("i", "<C-j>", 'copilot#Accept("<CR>")',
+            { desc = "Accept Copilot suggestion", expr = true, replace_keycodes = false })
     end,
 }
