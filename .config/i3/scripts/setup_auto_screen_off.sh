157244f78321980a8c751eb8bc75d9f08fa79e3a
Jonas Weber <jonas@jole.dev>
2024-03-29 23:05:21 +0100

    i3 auto screen off stuff

--- .config/i3/scripts/setup_auto_screen_off.sh
+++ .config/i3/scripts/setup_auto_screen_off.sh
@@ -2,6 +2,5 @@
 
 TIMEOUT=900
 
-# Define the number of seconds of inactivity before the screen is turned off and the session is locked
-xset dpms $TIMEOUT $TIMEOUT $TIMEOUT
+# Set the number of seconds of inactivity before the screen saver is activated (which also locks the session)
 xset s $TIMEOUT $TIMEOUT
