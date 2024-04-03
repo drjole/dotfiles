e96da12356cfa38895e3d825f8cde38d7ec24a51
Jonas Weber <jonas@jole.dev>
2024-04-03 09:53:11 +0200

    Enable auto formatting on save by none-ls in Neovim

--- .config/nvim/lua/jole/plugins/none-ls.lua
+++ .config/nvim/lua/jole/plugins/none-ls.lua
@@ -2,15 +2,30 @@ return {
     "nvimtools/none-ls.nvim",
     config = function()
         local null_ls = require("null-ls")
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
         null_ls.setup({
             sources = {
                 null_ls.builtins.diagnostics.erb_lint,
-                null_ls.builtins.formatting.erb_format,
+                null_ls.builtins.formatting.erb_format.with({
+                    extra_args = { "--print-width", "120" },
+                }),
                 null_ls.builtins.formatting.prettier,
                 null_ls.builtins.formatting.shfmt.with({
                     extra_args = { "-i", "2" },
                 }),
             },
+            on_attach = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({ async = false })
+                        end,
+                    })
+                end
+            end,
         })
     end,
 }
