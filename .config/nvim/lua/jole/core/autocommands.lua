296e8e8294e0260b5fd55d0ff5096a42dbfcf632
Jonas Weber <jonas@jole.dev>
2024-04-03 09:52:12 +0200

    Re-enable ruby indentation bug fix in Neovim

--- .config/nvim/lua/jole/core/autocommands.lua
+++ .config/nvim/lua/jole/core/autocommands.lua
@@ -17,9 +17,9 @@ vim.api.nvim_create_autocmd("FileType", {
 
 -- Ruby indent bug
 -- https://github.com/nvim-treesitter/nvim-treesitter/issues/3363
--- vim.api.nvim_create_autocmd("FileType", {
---     pattern = { "ruby" },
---     callback = function()
---         vim.cmd.setlocal({ "indentkeys-=." })
---     end,
--- })
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "ruby" },
+    callback = function()
+        vim.cmd.setlocal({ "indentkeys-=." })
+    end,
+})
