5d760c2446d959af1abff45abe4936dfb09502f6
Jonas Weber <jonas@jole.dev>
2024-04-17 19:07:02 +0200

    Add harpoon to Nvim

---
+++ .config/nvim/lua/jole/plugins/harpoon.lua
@@ -0,0 +1,32 @@
+return {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    config = function()
+        local harpoon = require("harpoon")
+
+        harpoon:setup()
+
+        vim.keymap.set("n", "<leader>a", function()
+            harpoon:list():add()
+        end, { desc = "Add a new harpoon mark" })
+        vim.keymap.set("n", "<C-e>", function()
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+        end, { desc = "Toggle harpoon menu" })
+
+        vim.keymap.set("n", "<C-j>", function()
+            harpoon:list():select(1)
+        end, { desc = "Select harpoon mark 1" })
+        vim.keymap.set("n", "<C-k>", function()
+            harpoon:list():select(2)
+        end, { desc = "Select harpoon mark 2" })
+        vim.keymap.set("n", "<C-l>", function()
+            harpoon:list():select(3)
+        end, { desc = "Select harpoon mark 3" })
+        vim.keymap.set("n", "<C-;>", function()
+            harpoon:list():select(4)
+        end, { desc = "Select harpoon mark 4" })
+    end,
+}
