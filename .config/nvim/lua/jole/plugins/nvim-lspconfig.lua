85c12200d2085751890afc890f4efa13c66f9924
Jonas Weber <jonas@jole.dev>
2024-07-19 13:04:28 +0200

    Add LSP inlay hints and re-configure rust-analyzer

--- .config/nvim/lua/jole/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/jole/plugins/nvim-lspconfig.lua
@@ -9,8 +9,7 @@ return {
 
         -- Custom on_attach callback that includes an autocommand to format on save
         local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-        local on_attach = function(_, bufnr)
-            -- Format on save
+        local on_attach = function(client, bufnr)
             vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
             vim.api.nvim_create_autocmd("BufWritePre", {
                 group = augroup,
@@ -71,16 +70,11 @@ return {
             settings = {
                 ["rust-analyzer"] = {
                     cargo = {
-                        allFeatures = true,
+                        features = "all",
                     },
                     check = {
                         command = "clippy",
-                    },
-                    imports = {
-                        prefix = "crate",
-                        granularity = {
-                            enforce = true,
-                        },
+                        extraArgs = { "--", "-W", "clippy::pedantic", "-W", "clippy::nursery", "-W", "clippy::unwrap_used", "-W", "clippy::expect_used" },
                     },
                     procMacro = {
                         ignored = {
@@ -159,62 +153,71 @@ return {
         -- after the language server attaches to the current buffer
         vim.api.nvim_create_autocmd("LspAttach", {
             group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-            callback = function(ev)
+            callback = function(event)
                 local builtin = require("telescope.builtin")
 
                 -- Enable completion triggered by <c-x><c-o>
-                vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+                vim.bo[event.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
                 -- Buffer local mappings.
                 -- See `:help vim.lsp.*` for documentation on any of the below functions
-                local opts = { desc = "LSP: Go to declaration", buffer = ev.buf }
+                local opts = { desc = "LSP: Go to declaration", buffer = event.buf }
                 vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 
-                opts = { desc = "LSP: Go to definition", buffer = ev.buf }
+                opts = { desc = "LSP: Go to definition", buffer = event.buf }
                 vim.keymap.set("n", "gd", builtin.lsp_definitions, opts)
 
-                opts = { desc = "LSP: Hover", buffer = ev.buf }
+                opts = { desc = "LSP: Hover", buffer = event.buf }
                 vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 
-                opts = { desc = "LSP: Go to implementations", buffer = ev.buf }
+                opts = { desc = "LSP: Go to implementations", buffer = event.buf }
                 vim.keymap.set("n", "gi", builtin.lsp_implementations, opts)
 
-                opts = { desc = "LSP: Signature help", buffer = ev.buf }
+                opts = { desc = "LSP: Signature help", buffer = event.buf }
                 vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help, opts)
 
-                opts = { desc = "LSP: Add workspace folder", buffer = ev.buf }
+                opts = { desc = "LSP: Add workspace folder", buffer = event.buf }
                 vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
 
-                opts = { desc = "LSP: Remove workspace folder", buffer = ev.buf }
+                opts = { desc = "LSP: Remove workspace folder", buffer = event.buf }
                 vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
 
-                opts = { desc = "LSP: List workspace folders", buffer = ev.buf }
+                opts = { desc = "LSP: List workspace folders", buffer = event.buf }
                 vim.keymap.set("n", "<leader>wl", function()
                     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
                 end, opts)
 
-                opts = { desc = "LSP: Go to type definitions", buffer = ev.buf }
+                opts = { desc = "LSP: Go to type definitions", buffer = event.buf }
                 vim.keymap.set("n", "<leader>D", builtin.lsp_type_definitions, opts)
 
-                opts = { desc = "LSP: Rename", buffer = ev.buf }
+                opts = { desc = "LSP: Rename", buffer = event.buf }
                 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 
-                opts = { desc = "LSP: Code actions", buffer = ev.buf }
+                opts = { desc = "LSP: Code actions", buffer = event.buf }
                 vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
 
-                opts = { desc = "LSP: Go to references", buffer = ev.buf }
+                opts = { desc = "LSP: Go to references", buffer = event.buf }
                 vim.keymap.set("n", "gr", builtin.lsp_references, opts)
 
-                opts = { desc = "LSP: Format", buffer = ev.buf }
+                opts = { desc = "LSP: Format", buffer = event.buf }
                 vim.keymap.set("n", "<leader>f", function()
                     vim.lsp.buf.format({ async = false })
                 end, opts)
 
-                opts = { desc = "LSP: Document symbols", buffer = ev.buf }
+                opts = { desc = "LSP: Document symbols", buffer = event.buf }
                 vim.keymap.set("n", "<leader>ds", builtin.lsp_document_symbols, opts)
 
-                opts = { desc = "LSP: Workspace symbols", buffer = ev.buf }
+                opts = { desc = "LSP: Workspace symbols", buffer = event.buf }
                 vim.keymap.set("n", "<leader>ws", builtin.lsp_dynamic_workspace_symbols, opts)
+
+                -- Inlay hints for supporting LSP servers
+                local client = vim.lsp.get_client_by_id(event.data.client_id)
+                if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+                    opts = { desc = "LSP: Toggle inlay hints", buffer = event.buf }
+                    vim.keymap.set("n", "<leader>th", function()
+                        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+                    end, opts)
+                end
             end,
         })
     end,
