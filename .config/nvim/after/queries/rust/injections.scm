ba1f05592eb334b7743e08b4813379399b828ad9
Jonas Weber <jonas@jole.dev>
2024-07-23 23:12:50 +0200

    Expand rust tree-sitter injections

--- .config/nvim/after/queries/rust/injections.scm
+++ .config/nvim/after/queries/rust/injections.scm
@@ -3,12 +3,35 @@
 (macro_invocation
   (scoped_identifier
     path: (identifier) @path (#eq? @path "sqlx")
-    name: (identifier) @name (#match? @name "^(query|query_as)$")
-  )
+    name: (identifier) @name (#match? @name "^query|query_as$")
+    )
   (token_tree
-    (raw_string_literal
-      (string_content) @injection.content
-      (#set! injection.language "sql")
+    [
+     (string_literal
+       (string_content) @injection.content
+       )
+     (raw_string_literal
+       (string_content) @injection.content
+       )
+     ]
+    (#set! injection.language "sql")
     )
   )
-)
+
+(call_expression
+  (scoped_identifier
+    path: (identifier) @path (#eq? @path "sqlx")
+    name: (identifier) @name (#match? @name "^query|query_as$")
+    )
+  (arguments
+    [
+     (string_literal
+       (string_content) @injection.content
+       )
+     (raw_string_literal
+       (string_content) @injection.content
+       )
+     ]
+    (#set! injection.language "sql")
+    )
+  )
