3eb1610c84007d884e9c9b0c7a95f9b31b18d0f7
Jonas Weber <jonas@jole.dev>
2024-10-03 17:43:11 +0200

    Show relative paths in oil

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,8 +1,14 @@
 function _G.get_oil_winbar()
     local dir = require("oil").get_current_dir()
     if dir then
-        return vim.fn.fnamemodify(dir, ":~")
+        dir = dir:gsub("/$", "")
+        local cwd = vim.fn.getcwd()
+        if dir == cwd then
+            return vim.fn.fnamemodify(dir, ":~:."):gsub("^" .. cwd, ".")
+        end
+        return vim.fn.fnamemodify(dir, ":~:."):gsub("^" .. cwd, ".")
     else
+        -- If there is no current directory (e.g. over ssh), just show the buffer name
         return vim.api.nvim_buf_get_name(0)
     end
 end
