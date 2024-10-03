3ebf37a22373bb9ad72628476f6d39b1bf789008
Jonas Weber <jonas@jole.dev>
2024-10-03 17:29:20 +0200

    Show CWD in winbar in oil

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,3 +1,12 @@
+function _G.get_oil_winbar()
+    local dir = require("oil").get_current_dir()
+    if dir then
+        return vim.fn.fnamemodify(dir, ":~")
+    else
+        return vim.api.nvim_buf_get_name(0)
+    end
+end
+
 return {
     "stevearc/oil.nvim",
     dependencies = {
@@ -7,6 +16,9 @@ return {
         },
     },
     opts = {
+        win_options = {
+            winbar = "%!v:lua.get_oil_winbar()",
+        },
         view_options = {
             show_hidden = true,
         },
