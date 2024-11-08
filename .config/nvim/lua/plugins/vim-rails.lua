8fa0c50105c9343832e8b304af0eaf83780270ef
Jonas Weber <jonas@jole.dev>
2024-11-08 13:44:52 +0100

    Set yml file type for locales in rails

--- .config/nvim/lua/plugins/vim-rails.lua
+++ .config/nvim/lua/plugins/vim-rails.lua
@@ -1,6 +1,17 @@
 return {
     "tpope/vim-rails",
     config = function()
+        vim.api.nvim_create_autocmd(
+            { "BufNewFile", "BufReadPost" },
+            {
+                pattern = { "*.yml" },
+                callback = function()
+                    vim.bo.filetype = "yaml"
+                end,
+
+            }
+        )
+
         vim.keymap.set("n", "<leader>rm", "<cmd>Emodel<CR>", { desc = "Go to Rails model" })
         vim.keymap.set("n", "<leader>rv", "<cmd>Eview<CR>", { desc = "Go to Rails view" })
         vim.keymap.set("n", "<leader>rc", "<cmd>Econtroller<CR>", { desc = "Go to Rails controller" })
