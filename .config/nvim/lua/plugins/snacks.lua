2572db19826eeae790f94c98782ddf0826a47b86
Jonas Weber <jonas@jole.dev>
2024-11-09 12:18:40 +0100

    Add snacks.nvim

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,20 @@
+return {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+        bigfile = { enabled = false },
+        notifier = { enabled = false },
+        quickfile = { enabled = false },
+        statuscolumn = { enabled = false },
+        words = { enabled = false },
+    },
+    keys = {
+        { "<leader>bd", function() Snacks.bufdelete() end,        desc = "Delete Buffer" },
+        { "<leader>gg", function() Snacks.lazygit() end,          desc = "Lazygit" },
+        { "<leader>gb", function() Snacks.git.blame_line() end,   desc = "Git Blame Line" },
+        { "<leader>gB", function() Snacks.gitbrowse() end,        desc = "Git Browse" },
+        { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
+        { "<leader>gl", function() Snacks.lazygit.log() end,      desc = "Lazygit Log (cwd)" },
+    },
+}
