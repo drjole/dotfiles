e92b7e72b0b0c085e539dc7d7b83bc714c1c2ba5
Jonas Weber <jonas@jole.dev>
2024-11-13 14:06:07 +0100

    Disable Copilot by default

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,10 +1,11 @@
 return {
     "github/copilot.vim",
-    cmd = "Copilot",
     keys = {
         { "<C-j>", 'copilot#Accept("<cr>")', mode = "i", desc = "Accept Copilot suggestion", expr = true, replace_keycodes = false },
     },
+    cmd = "Copilot",
     config = function()
         vim.g.copilot_no_tab_map = true
+        vim.cmd([[Copilot disable]])
     end,
 }
