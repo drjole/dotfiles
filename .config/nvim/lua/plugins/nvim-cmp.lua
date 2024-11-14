5ae8652a2e5ae22d236852818ddf5bc38ea12e18
Jonas Weber <jonas@jole.dev>
2024-11-14 11:04:25 +0100

    Fix luasnip setup

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -5,7 +5,10 @@ return {
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-cmdline",
-        "L3MON4D3/LuaSnip",
+        {
+            "L3MON4D3/LuaSnip",
+            build = "make install_jsregexp",
+        },
         "saadparwaiz1/cmp_luasnip",
         {
             "roobert/tailwindcss-colorizer-cmp.nvim",
@@ -69,5 +72,57 @@ return {
             }),
             matching = { disallow_symbol_nonprefix_matching = false },
         })
+
+        local ls = require "luasnip"
+
+        vim.snippet.expand = ls.lsp_expand
+
+        ---@diagnostic disable-next-line: duplicate-set-field
+        vim.snippet.active = function(filter)
+            filter = filter or {}
+            filter.direction = filter.direction or 1
+
+            if filter.direction == 1 then
+                return ls.expand_or_jumpable()
+            else
+                return ls.jumpable(filter.direction)
+            end
+        end
+
+        ---@diagnostic disable-next-line: duplicate-set-field
+        vim.snippet.jump = function(direction)
+            if direction == 1 then
+                if ls.expandable() then
+                    return ls.expand_or_jump()
+                else
+                    return ls.jumpable(1) and ls.jump(1)
+                end
+            else
+                return ls.jumpable(-1) and ls.jump(-1)
+            end
+        end
+
+        vim.snippet.stop = ls.unlink_current
+
+        -- ================================================
+        --      My Configuration
+        -- ================================================
+        ls.config.set_config {
+            history = true,
+            updateevents = "TextChanged,TextChangedI",
+            override_builtin = true,
+        }
+
+        for _, ft_path in ipairs(vim.api.nvim_get_runtime_file("lua/snippets/*.lua", true)) do
+            loadfile(ft_path)()
+        end
+
+        vim.keymap.set({ "i", "s" }, "<c-k>", function()
+            return vim.snippet.active { direction = 1 } and vim.snippet.jump(1)
+        end, { silent = true })
+
+        vim.keymap.set({ "i", "s" }, "<c-j>", function()
+            return vim.snippet.active { direction = -1 } and vim.snippet.jump(-1)
+        end, { silent = true })
     end,
 }
