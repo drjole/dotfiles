0b868f5d1ab7b21cf2261de3d6e12d313f26d2ca
Jonas Weber <jonas@jole.dev>
2024-12-30 16:23:04 +0100

    Add topiary formatter

--- .config/nvim/after/queries/rust/injections.scm
+++ .config/nvim/after/queries/rust/injections.scm
@@ -2,36 +2,40 @@
 
 (macro_invocation
   (scoped_identifier
-    path: (identifier) @path (#eq? @path "sqlx")
-    name: (identifier) @name (#match? @name "^query|query_as$")
-    )
+    path: (identifier) @path
+    (#eq? @path "sqlx")
+    name: (identifier) @name
+    (#match? @name "^query|query_as$")
+  )
   (token_tree
     [
-     (string_literal
-       (string_content) @injection.content
-       )
-     (raw_string_literal
-       (string_content) @injection.content
-       )
-     ]
+      (string_literal
+        (string_content) @injection.content
+      )
+      (raw_string_literal
+        (string_content) @injection.content
+      )
+    ]
     (#set! injection.language "sql")
-    )
   )
+)
 
 (call_expression
   (scoped_identifier
-    path: (identifier) @path (#eq? @path "sqlx")
-    name: (identifier) @name (#match? @name "^query|query_as$")
-    )
+    path: (identifier) @path
+    (#eq? @path "sqlx")
+    name: (identifier) @name
+    (#match? @name "^query|query_as$")
+  )
   (arguments
     [
-     (string_literal
-       (string_content) @injection.content
-       )
-     (raw_string_literal
-       (string_content) @injection.content
-       )
-     ]
+      (string_literal
+        (string_content) @injection.content
+      )
+      (raw_string_literal
+        (string_content) @injection.content
+      )
+    ]
     (#set! injection.language "sql")
-    )
   )
+)
--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -6,9 +6,17 @@ return {
         command = "npx",
         args = { "tailwindcss-class-sorter-erb" },
       },
+      topiary = {
+        command = "topiary",
+        args = { "format", "$FILENAME" },
+        stdin = false,
+      },
     },
     formatters_by_ft = {
       eruby = { "tailwindcss_class_sorter_erb", "erb_format" },
+      html = { "topiary" },
+      query = { "topiary" },
+      toml = { "topiary" },
     },
     format_on_save = {
       timeout_ms = 1000,
