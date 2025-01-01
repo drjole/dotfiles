28da06e32f70053d0aa7c696628e1a0e58c384d7
Jonas Weber <jonas@jole.dev>
2025-01-01 17:41:07 +0100

    Load vim-rails non-lazily

--- .config/nvim/lua/plugins/vim-rails.lua
+++ .config/nvim/lua/plugins/vim-rails.lua
@@ -1,13 +1,12 @@
 return {
   "tpope/vim-rails",
-  ft = {
-    "eruby",
-    "ruby",
-  },
   keys = {
     { "<leader>rc", "<cmd>Econtroller<cr>" },
     { "<leader>rh", "<cmd>Ehelper<cr>" },
     { "<leader>rm", "<cmd>Emodel<cr>" },
     { "<leader>rv", "<cmd>Eview<cr>" },
   },
+  lazy = false,
+  config = function()
+  end,
 }
