cc9f91ef28fa13ff19d47c1995ea623a0f1cfe4d
Jonas Weber <jonas@jole.dev>
2025-04-07 16:34:49 +0200

    Hide gitignored files in Oil

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,3 +1,4 @@
+-- https://github.com/stevearc/oil.nvim/blob/master/doc/recipes.md#show-cwd-in-the-winbar
 function _G.get_oil_winbar()
   local dir = require("oil").get_current_dir()
   if dir then
@@ -13,6 +14,48 @@ function _G.get_oil_winbar()
   end
 end
 
+-- https://github.com/stevearc/oil.nvim/blob/master/doc/recipes.md#hide-gitignored-files-and-show-git-tracked-hidden-files
+-- helper function to parse output
+local function parse_output(proc)
+  local result = proc:wait()
+  local ret = {}
+  if result.code == 0 then
+    for line in vim.gsplit(result.stdout, "\n", { plain = true, trimempty = true }) do
+      -- Remove trailing slash
+      line = line:gsub("/$", "")
+      ret[line] = true
+    end
+  end
+  return ret
+end
+
+-- build git status cache
+local function new_git_status()
+  return setmetatable({}, {
+    __index = function(self, key)
+      local ignore_proc = vim.system(
+        { "git", "ls-files", "--ignored", "--exclude-standard", "--others", "--directory" },
+        {
+          cwd = key,
+          text = true,
+        }
+      )
+      local tracked_proc = vim.system({ "git", "ls-tree", "HEAD", "--name-only" }, {
+        cwd = key,
+        text = true,
+      })
+      local ret = {
+        ignored = parse_output(ignore_proc),
+        tracked = parse_output(tracked_proc),
+      }
+
+      rawset(self, key, ret)
+      return ret
+    end,
+  })
+end
+local git_status = new_git_status()
+
 return {
   "stevearc/oil.nvim",
   dependencies = {
@@ -23,13 +66,28 @@ return {
   },
   lazy = false,
   opts = {
+    extra_scp_args = { "-4" },
     default_file_explorer = true,
     skip_confirm_for_simple_edits = true,
     win_options = {
       winbar = "%!v:lua.get_oil_winbar()",
     },
     view_options = {
-      show_hidden = true,
+      is_hidden_file = function(name, bufnr)
+        local dir = require("oil").get_current_dir(bufnr)
+        local is_dotfile = vim.startswith(name, ".") and name ~= ".."
+        -- if no local directory (e.g. for ssh connections), just hide dotfiles
+        if not dir then
+          return is_dotfile
+        end
+        -- dotfiles are considered hidden unless tracked
+        if is_dotfile then
+          return not git_status[dir].tracked[name]
+        else
+          -- Check if file is gitignored
+          return git_status[dir].ignored[name]
+        end
+      end,
       natural_order = true,
     },
   },
