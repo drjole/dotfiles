9d53f7273302725593e92fd5863559fc05a5f8e0
Jonas Weber <jonas@jole.dev>
2025-08-23 17:41:09 +0200

    Add treesitter-context

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,9 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     main = "nvim-treesitter.configs",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter-context",
+    },
     opts = {
         ensure_installed = {
             "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline",
