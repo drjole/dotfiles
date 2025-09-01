6472bff978bef1a5047ed8289d6f79e9ad5bd2b0
Jonas Weber <jonas@jole.dev>
2025-09-01 17:46:06 +0200

    Add tailwindcss-class-sorter back and herb symlinks

--- shared/.config/nvim/lua/plugins/conform.lua
+++ shared/.config/nvim/lua/plugins/conform.lua
@@ -3,14 +3,25 @@ return {
     opts = {
         formatters_by_ft = {
             css = { "prettier" },
+            eruby = { "tailwindcss_class_sorter_erb", lsp_format = "last" },
             javascript = { "prettier" },
             json = { "prettier" },
             markdown = { "prettier" },
             typescript = { "prettier" },
         },
-        format_on_save = {
-            timeout_ms = 500,
+        default_format_opts = {
             lsp_format = "fallback",
+        },
+        format_on_save = {},
+        formatters = {
+            tailwindcss_class_sorter_erb = {
+                command = "node_modules/.bin/tailwindcss-class-sorter-erb",
+                condition = function(_, _)
+                    local filename = vim.fn.expand("%:t")
+                    local ending = ".html.erb"
+                    return string.sub(filename, - #ending) == ending
+                end,
+            },
         },
     },
 }
